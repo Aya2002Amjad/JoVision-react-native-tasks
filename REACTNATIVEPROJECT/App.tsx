c6d9ee8e1cf6a04816f80ef8c80c6a903f5c95e0
Aya2002Amjad <aya2002amjad@gmail.com>
2025-02-05 14:40:27 +0300

    add task17

--- REACTNATIVEPROJECT/App.tsx
+++ REACTNATIVEPROJECT/App.tsx
@@ -1,5 +1,9 @@
 import Task16 from "./Tasks/task16";
+import Task17 from "./Tasks/task17";
 
 export default function App(){
-  return (<Task16></Task16>);
+  return (
+  //<Task16></Task16>
+  <Task17></Task17>
+);
 }
